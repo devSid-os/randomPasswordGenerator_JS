6f90e758d144662c5e5ebd098a24a3093285becc
devSid-os <siddjain341@gmail.com>
2024-02-11 12:34:41 +0530

    added js file

---
+++ src/script.js
@@ -0,0 +1,12 @@
+const randomPasswordInput = document.getElementById("randomPasswordInput");
+const passwordLengthIndicator = document.getElementById("passwordLengthIndicator");
+const refreshBtn = document.getElementById("refreshBtn");
+const passwordLength = document.getElementById("passwordLength");
+const minusBtn = document.getElementById("minusBtn");
+const plusBtn = document.getElementById("plusBtn");
+const passwordRange = document.getElementById("passwordRange");
+const capitalLettersCheck = document.getElementById("capitalLetters");
+const smallLettersCheck = document.getElementById("smallLetters");
+const numbersCheck = document.getElementById("numbers");
+const symbolsCheck = document.getElementById("symbols");
+
